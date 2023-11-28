21959322cea78b91c0d0622ae85f9e8286327d38
Masum Kazi <88852649+masumKazibd@users.noreply.github.com>
2023-11-28 04:15:50 +0000

    Month name

---
+++ JavaScripts/Find the month name.js
@@ -0,0 +1,40 @@
+var number=prompt('Enter a number : ');
+number = Number(number);
+switch(number){
+    case(1):
+         console.log('January');   
+         break;
+    case(2):
+        console.log('February');
+        break;
+    case(3):
+        console.log('March');
+        break;
+    case(4):
+        console.log('April');
+        break;
+    case(5):
+        console.log('May');
+        break;
+    case(6):
+        console.log('June');
+        break;
+    case(7):
+        console.log('July');
+    case(8):
+        console.log('August');
+    case(9):
+        console.log('September');
+        break;
+    case(10):
+        console.log('October');
+        break;
+    case(11):
+        console.log('November');
+        break;
+    case(12):
+        console.log('December');
+        break;
+    default:
+        console.log('Please input 1 to 12');
+}
